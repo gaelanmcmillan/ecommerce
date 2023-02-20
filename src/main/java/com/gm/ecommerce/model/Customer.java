9aef18396f7c0d67e8f7b4aee376a05075449e23
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-02-20 12:24:43 -0500

    initialize with Spring Initialzr, add Customer

---
+++ src/main/java/com/gm/ecommerce/model/Customer.java
@@ -0,0 +1,70 @@
+package com.gm.ecommerce.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ *  <b>Customer</b> defines a registered customer. It keeps track of the customer's name and address.
+ *  A unique id is generated when a new customer is created.
+ */
+@Entity
+class Customer {
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String address;
+
+    public Customer () {}
+    Customer (String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(address, customer.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
