756096666becb6a0cf04a9a3a0d069b8e0ceaf1a
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-02-20 15:26:35 -0500

    add documentation to assembler

--- src/main/java/com/gm/ecommerce/assembler/GenericAssembler.java
+++ src/main/java/com/gm/ecommerce/assembler/GenericAssembler.java
@@ -1,13 +1,21 @@
 package com.gm.ecommerce.assembler;
 import java.util.function.Function;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
-import org.springframework.stereotype.Component;
 
+/**
+ * <code>GenericAssembler&lt;T&gt;</code> facilitates simplifying the RESTful practice of
+ * adding <a href="https://www.rfc-editor.org/rfc/rfc8288">web links</a> to API endpoints.
+ * The utility of an assembler class is demonstrated well in
+ * <a href="https://spring.io/guides/tutorials/rest/#:~:text=Simplifying%20Link%20Creation">this Spring tutorial</a>.
+ * <br/><br/>
+ *
+ * The basic purpose of this class is to expose a function <code>GenericAssembler&lt;T&gt;::toModel(T)</code> which converts
+ * an instance of type T to an instance of type <code>EntityModel&lt;T&gt;</code>. Having this utility in generic form
+ * means we can cut down significantly on the number of Assembler classes we make.
+ * */
 public class GenericAssembler <T> implements RepresentationModelAssembler <T, EntityModel<T>> {
-
     private final Function<T, EntityModel<T>> modelCallback;
 
     public GenericAssembler (Function<T, EntityModel<T>> modelCallback) {
